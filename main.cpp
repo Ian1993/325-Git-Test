ee72b049157cc0330552f370aa115955c1e4dd56
Ian Nevills <ianmnevi@ut.utm.edu>
2018-04-02 20:30:19 -0500

    Third Commit: Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,34 @@
 
 using namespace std;
 
+int usersum(int n);
+
 int main(){
 
   cout << "Hello World!" << endl;
+
+  cout << "Input a number" << endl;
+
+  int x;
+
+  cin >> x;
+
+  int y = usersum(x);
+
+  cout << "Your Number's sum from 1 to " << x << " is: " << y << endl;
+
+  
+  
+}
+
+int usersum(int n){
+  int z = 0;
+  for(int i = 1; i <= n ; i++){
+
+    z = z + i;
+    
+  }
+
+  return z;
   
 }
